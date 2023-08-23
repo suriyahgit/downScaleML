f7af0fb5853159f0c632262cb2f9bda3d4a99da5
Dhinakaran Suriyah <sdhinakaran@eosao22.eurac.edu>
2023-08-23 14:56:02 +0200

    Correcting model_search.sh file

--- downscaleml/main/inputoutput.py
+++ downscaleml/main/inputoutput.py
@@ -1,13 +1,14 @@
 import pathlib
 
-
+ERA5 = "p_REANALYSIS"
+#ERA5 = "REANALYSIS"
 
 ROOT = pathlib.Path('/mnt/CEPH_PROJECTS/InterTwin/Climate_Downscaling/larger_alps/')
 
 # path to this file
 HERE = pathlib.Path(__file__).parent
 
-ERA5_PATH = ROOT.joinpath('REANALYSIS')
+ERA5_PATH = ROOT.joinpath(ERA5)
 
 OBS_PATH = ROOT.joinpath('CERRA')
 
@@ -17,4 +18,4 @@ RESULTS = pathlib.Path('/mnt/CEPH_PROJECTS/InterTwin/Climate_Downscaling/larger_
 
 MODEL_PATH = RESULTS.joinpath('ml_Models')
 
-TARGET_PATH = RESULTS.joinpath('ml_Predictions')
+TARGET_PATH = RESULTS.joinpath('grid_search')
